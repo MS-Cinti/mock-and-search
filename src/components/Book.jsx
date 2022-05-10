86734e73dd674fafc54ebc8ba2aff8edf4cd8f87
MS-Cinti <szabadicintia@gmail.com>
2022-05-10 13:56:15 +0200

    added some changes

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Books() {
+function Book() {
   return (
     <div>Books</div>
   )
 }
 
-export default Books
+export default Book
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import mockServer from './mockServer';
-import Books from './components/Books';
+import Book from './components/Book';
 import LoadingMask from './components/LoadingMask';
 
 mockServer()
